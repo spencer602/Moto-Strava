8e2467311617b0c6885de8d287acf98191fa5c31
Spencer DeBuf <spencer602@gmail.com>
2019-11-18 15:07:00 -0700

    Initial Commit

---
+++ Moto Strava/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Moto Strava
+//
+//  Created by Spencer DeBuf on 11/18/19.
+//  Copyright © 2019 Spencer DeBuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
