045b356840415134a221fe3d22e361e2e9299be3
Spencer DeBuf <spencer602@gmail.com>
2019-11-25 16:44:38 -0700

    added editDetailDataViewController, wired up segue from dataTableViewCell

---
+++ Moto Strava/EditDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditDetailViewController.swift
+//  Moto Strava
+//
+//  Created by Spencer DeBuf on 11/25/19.
+//  Copyright © 2019 Spencer DeBuf. All rights reserved.
+//
+
+import UIKit
+
+class EditDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
